e2dc62c7c32cdb435b77e37c90678860c869353d
高小川 <419055454@qq.com>
2015-10-19 20:58:09 +0800

    change style & base journal page logic

--- page_fund/page_script.js
+++ page_fund/page_script.js
@@ -24,6 +24,7 @@ face_room.prototype.extend({
     }
 });
 
+//method page (404 500 etc.)
 function method_room(){}
 method_room.prototype = new base_page();
 method_room.prototype.extend({
@@ -33,23 +34,73 @@ method_room.prototype.extend({
     routing : function(parameters){
         switch(parameters.page){
             case '404':
-                checkout_content('page_fund/html/404.html');
+                checkout_content_use_html(cache_strock.get_data("404"));
                 break;
             case '500':
-                checkout_content('page_fund/html/500.html');
+                checkout_content_use_html(cache_strock.get_data("500"));
                 break;
         }
     }
 });
 
-function journal_room(){}
+//journal page involve theme list render & render journal content (html) & render markdown to journal content
+function journal_room(){
+    //theme list json data method
+    this.journal_list = {};
+    //all journal theme list indexs
+    this.journal_theme_indexs = {};
+    //replace flag set
+    this.replace_flag_set = {};
+
+    //render list page by theme
+    this.render_list_model = function(theme){
+        var render_list = this.journal_theme_indexs[theme];
+        
+    }
+
+    //construct content that render markdown to content model and return content
+
+    //***need code***
+
+    //load markdown content
+
+    //***need code***
+
+}
 journal_room.prototype = new base_page();
 journal_room.prototype.extend({
     on_init : function(){
-        checkout_content('page_fund/html/journal_list_model.html');
-    },
+        //load journal theme list model
+        this.journal_theme_indexs = cache_strock.get_data("journal-theme");
 
+        //load content replace flags key-value pairs
+        this.replace_flag_set = cache_strock.get_data("replace-flag");
+
+    },
     routing : function(parameters){
-        
+        switch(parameters.page){
+            case "list":
+                //set default value
+                if(parameters["theme"] === undefined){
+                    parameters["theme"] = "default";
+                }
+
+                //render journal model list by theme
+                checkout_content_use_html(cache_strock.get_data("journal-list-model"));
+
+                //render list page model
+                this.render_list_model(parameters["theme"]);
+                break;
+            case "journal":
+                //render journal content by journal identity
+
+                //***need code***
+
+                break;
+            default:
+                //not found
+                checkout_content('page_fund/html/404.html');
+                break;
+        }
     }
 })
